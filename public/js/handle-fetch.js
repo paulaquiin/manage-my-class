3d56eb0095124f99b20285f7e29ab1e2f0e66c41
Paula Quintana <paulaquintanagon00@gmail.com>
2025-10-10 18:26:48 +0200

    Feat: Nueva función para añadir al fetch nativo un control de usuarios

---
+++ public/js/handle-fetch.js
@@ -0,0 +1,30 @@
+export async function handleFetch(url, method, body) {
+
+    const token = localStorage.getItem("token");
+    if (!token) {
+        localStorage.removeItem("token");
+        window.location.href = "/iniciar-sesion";
+        return;
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: method,
+            headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}`},
+            body: body
+        });
+
+        // En el caso de una respuesta no autorizada (401) se redirige
+        // a iniciar-sesion
+        if (response.status === 401) {
+            localStorage.removeItem("token");
+            window.location.href = "/iniciar-sesion";
+            return;
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (err) {
+        console.error("Error en fetch:", err);
+    }
+}
