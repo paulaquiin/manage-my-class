5dc9e0185ac45a6c639b5d2f310567d4d56a84f9
Paula Quintana <paulaquintanagon00@gmail.com>
2025-10-13 17:05:54 +0200

    Feat: (Backend) Endpoint para obtener y añadir estudiantes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const db = require("./database");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const JWT_SECRET_KEY = "1c975aca040f714db40ba0f0fdbf8aad"; // Firma generada en https://jwtsecrets.com/
 const JWT_EXPIRATION = "12h";
 
-// MIDDLEWARES
-app.use(express.static("public")); //Muestra los archivos de la carpeta public
-app.use(bodyParser.json()); // Permite utilizar estructuras JSON en cada petición
+/**
+ * MIDDLEWARES
+*/
+app.use(express.json({ limit: "10mb" })); // Permite utilizar estructuras JSON en cada petición y además le indico un tamaño mayor de contenido JSON de hasta 10MB (necesario para las fotos de estudiantes)
+app.use(express.static("public")); // Sirve los archivos de la carpeta public
+// Permite comprobar si la petición recibida es de un usuario autenticado
+// Este middleware se ejecutará en todas las peticiones donde sea necesario la autenticación
 function authJwtToken(req, res, next) {
     const exclude = ["/api/register", "/api/login"];
     if (exclude.includes(req.path)) return next();
@@ -20,17 +24,17 @@ function authJwtToken(req, res, next) {
     const token = authHeader && authHeader.split(" ")[1];
     if (!token) return res.status(401).json({ message: "No autorizado" });
 
-// Comprueba si la petición la hace un usuario autenticado y se ejecuta en todas las peticiones que sean necesarias
     jwt.verify(token, JWT_SECRET_KEY, (err, user) => {
         if (err) return res.status(403).json({ message: "Token inválido" });
         req.user = user;
         next();
     });
 }
-
 app.use(authJwtToken);
 
-// ENDPOINTS
+/**
+ * ENDPOINTS
+*/
 
 // Endpoint para registrar profesores
 app.post("/api/register/", async (req, res) => {
@@ -44,7 +48,7 @@ app.post("/api/register/", async (req, res) => {
         [user, hashedPwd, dni],
         function (error) {
             if (error) {
-                 return res.status(400).json({ success: false, errorId: "user-exists-error", message: "Usuario ya existe" });
+                return res.status(400).json({ success: false, errorId: "user-exists-error", message: "Usuario ya existe" });
             } else {
                 // Usuario registrado y redirecciona a la pantalla de inicio de sesión
                 return res.status(201).json({ success: true });
@@ -75,6 +79,7 @@ app.post("/api/login/", async (req, res) => {
         }
     );
 })
+
 
 // Endpoint para clases
 app.post("/api/class/", (req, res) => {
@@ -127,4 +132,50 @@ app.delete("/api/class/", (req, res) => {
     )
 })
 
-app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}`));
+
+// Endpoint para alumnos
+app.post("/api/student/", (req, res) => {
+    const { name, surname, photo, classId, userId } = req.body;
+    db.run(
+        "INSERT INTO students (name, surname, photo, class_id, user_id) VALUES (?, ?, ?, ?, ?)",
+        [name, surname, photo, classId, userId],
+        function (error) {
+            if (error) {
+                return res.status(400).json({ success: false });
+            } else {
+                return res.status(201).json({ success: true });
+            }
+        }
+    )
+})
+
+app.get("/api/student/", (req, res) => {
+    const userId = req.query.userId;
+    db.all(
+        `
+            SELECT s.name AS student_name,
+            s.surname,
+            s.photo,
+            s.class_id,
+            s.user_id,
+            c.id AS class_id,
+            c.name AS class_name,
+            c.grade,
+            c.icon
+            FROM students s LEFT JOIN classes c ON s.class_id = c.id
+            WHERE s.user_id = ?
+        `,
+        [userId],
+        function (error, rows) {
+            if (error) {
+                return res.status(400).json({ success: false });
+            } else {
+                return res.status(201).json({ success: true, rows });
+
+            }
+
+        }
+    );
+})
+
+app.listen(PORT, () => console.log(`Servidor escuchando en http://localhost:${PORT}`));
